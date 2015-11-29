11b2769f92918a1350e8a13785ad802167d5e2fe
reiki4040 <reiki4040@gmail.com>
2015-11-29 14:09:39 +0900

    updated rnssh to 0.3.7

--- rnssh.rb
+++ rnssh.rb
@@ -3,15 +3,15 @@ require "formula"
 class Rnssh < Formula
   desc "easy ssh login to EC2."
   homepage 'https://github.com/reiki4040/rnssh'
-  version "0.3.6"
+  version "0.3.7"
 
   if OS.mac?
     if Hardware.is_64_bit?
       url "https://github.com/reiki4040/rnssh/releases/download/v" + version + "/rnssh-" + version + "-darwin-amd64.zip"
-      sha256 '8bd237c1ec36010028c12f1fe6a69bd33892600a06a0dcc27019fcd6025d8960'
+      sha256 '913fe3634b4e68eecd1124ec0e13484e7e400f238c8e85ba666da11fa1bf819a'
     else
       url "https://github.com/reiki4040/rnssh/releases/download/v" + version + "/rnssh-" + version + "-darwin-386.zip"
-      sha256 'e42c5734c95c12f87eb4fd73012c2e6fd0c21581b50e8463db05bd052529140c'
+      sha256 '1a3e5842dfc70db1df43790e6c998b8b14a1ab5645616bf553e447cd320940b7'
     end
   end
 
@@ -36,9 +36,9 @@ class Rnssh < Formula
     export AWS_ACCESS_KEY_ID=YOUR_AWS_ACCESS_KEY
     export AWS_SECRET_ACCESS_KEY=YOUR_AWS_SECRET_ACCESS_KEY
 
-    # Option: you can set default aws region.
+    # set default aws region.
 
-    export AWS_REGION=ap-northeast-1
+	rnssh --init
 
 EOF
   end
